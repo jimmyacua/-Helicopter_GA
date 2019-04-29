015194b9c602a1c1086b944b4e00cfc1869444a4
Carlos Delgado <carlos.delgadorojas@ucr.ac.cr>
2019-04-29 15:24:05 -0600

    plantilla doc interna lista

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+/**
+ * @brief Clase principal para la ejecución del algoritmo genético.
+ *
+ * @author  Geovanny Cordero Valverde   (geovanny.corderovalverde@ucr.ac.cr)
+ * @author  Carlos Delgado Rojas        (carlos.delgadorojas@ucr.ac.cr)
+ * @author  Jimmy Acuña Díaz            (TODO agregar acá su email please)
+ */
 public class Main {
+    /**
+     * Main
+     * @param args args
+     */
     public static void main(String[] args){
-
+        // TODO
     }
 }
--- src/Obstaculo.java
+++ src/Obstaculo.java
@@ -1,20 +1,47 @@
 import java.util.ArrayList;
-
+/**
+ * @brief Clase que permite representar un Obstáculo en el contexto del algoritmo genético.
+ *
+ * @author  Geovanny Cordero Valverde   (geovanny.corderovalverde@ucr.ac.cr)
+ * @author  Carlos Delgado Rojas        (carlos.delgadorojas@ucr.ac.cr)
+ * @author  Jimmy Acuña Díaz            (TODO agregar acá su email please)
+ */
 public class Obstaculo {
+    /**
+     *
+     */
     private ArrayList<Integer> x;
+
+    /**
+     *
+     */
     private ArrayList<Integer> y;
 
+    /**
+     *
+     */
     public Obstaculo(){
         x = new ArrayList<>();
         y = new ArrayList<>();
     }
 
+    /**
+     *
+     * @param xs
+     * @param ys
+     */
     public Obstaculo(ArrayList<Integer> xs, ArrayList<Integer> ys){
         x = xs;
         y = ys;
     }
 
+    /**
+     *
+     * @param individuo
+     * @return
+     */
     public boolean hayInterseccion(Individuo individuo){
-        
+        //TODO
+        return false;
     }
 }
