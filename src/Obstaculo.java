b54b7d55ec5ed6914aba75cafca8978e0ba953e3
Carlos Delgado <carlos.delgadorojas@ucr.ac.cr>
2019-04-29 12:38:27 -0600

    repo listo para usarse en IntelliJ

---
+++ src/Obstaculo.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Obstaculo {
+    private ArrayList<Integer> x;
+    private ArrayList<Integer> y;
+
+    public Obstaculo(){
+        x = new ArrayList<>();
+        y = new ArrayList<>();
+    }
+
+    public Obstaculo(ArrayList<Integer> xs, ArrayList<Integer> ys){
+        x = xs;
+        y = ys;
+    }
+
+    public boolean hayInterseccion(Individuo individuo){
+        
+    }
+}
